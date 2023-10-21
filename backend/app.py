bf0d59a0d98e61ff4fcc3a0303a6e6cdd0f4eff4
Benson Mugure <b.mugure@alustudent.com>
2023-10-21 14:00:58 +0400

    sentiment analysis

--- backend/app.py
+++ backend/app.py
@@ -1,42 +1,36 @@
 from flask import Flask, request, jsonify
-import pandas as pd
-import os
-from textblob import TextBlob
+from sentiment_analysis import analyze_sentiment
+from data_handling import store_tasks_data, get_tasks_data, analyze_and_store_sentiment_analysis, get_sentiment_analysis_data
 
 app = Flask(__name__)
-
-# Function for sentiment analysis
-def analyze_sentiment(text):
-    analysis = TextBlob(text)
-    return analysis.sentiment.polarity
-
-# Store tasks data in a CSV file
-def store_tasks_data(data):
-    data_file = "tasks_data.csv"
-
-    if not os.path.exists(data_file):
-        # Create a new CSV file with headers if it doesn't exist
-        df = pd.DataFrame(columns=["Task name", "Assignee name", "Comments", "Sentiment Score"])
-        df.to_csv(data_file, index=False)
-
-    # Calculate sentiment scores and append new data to the CSV file
-    data["Sentiment Score"] = data["Comments"].apply(analyze_sentiment)
-    df = pd.read_csv(data_file)
-    df = df.append(data, ignore_index=True)
-    df.to_csv(data_file, index=False)
 
 @app.route("/tasks", methods=["POST"])
 def receive_tasks_data():
     data = request.json
-    # Store data in CSV file with sentiment scores
     store_tasks_data(data)
+    
+    # Perform sentiment analysis and store results
+    analyze_and_store_sentiment_analysis(data)
+    
     return "Data received and stored successfully"
+
+@app.route("/get_tasks_data", methods=["GET"])
+def get_tasks_data_route():
+    data = get_tasks_data()
+    return jsonify(data)
+
+@app.route("/get_sentiment_data", methods=["GET"])
+def get_sentiment_data_route():
+    data = get_sentiment_analysis_data()
+    return jsonify(data)
 
 @app.route("/analyze_sentiment", methods=["POST"])
 def analyze_sentiment_route():
     data = request.json
     text = data.get("text", "")
     sentiment_score = analyze_sentiment(text)
+
+    # Return sentiment score without storing it in sentiment_data
     return jsonify({"sentiment_score": sentiment_score})
 
 if __name__ == "__main__":
