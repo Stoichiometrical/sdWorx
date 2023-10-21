bf09ae5e8a2a4e9f6a5a4ff13e2abb5055aa62fa
Benson Mugure <b.mugure@alustudent.com>
2023-10-21 12:44:11 +0400

    app.py

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import EmployeeView from "./components/EmployeeView";
+import ManagerView from "./components/ManagerView";
+import Tasks from "./pages/Tasks";
+import Reports from "./pages/Reports";
+import CareerGrowth from "./pages/CareerGrowth";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        {/* Add side menus and navigation links here */}
+        <Switch>
+          <Route path="/tasks" component={Tasks} />
+          <Route path="/reports" component={Reports} />
+          <Route path="/career-growth" component={CareerGrowth} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
